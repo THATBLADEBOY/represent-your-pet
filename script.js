76c7524127c9811b38b917ba5ea0404aaf36b3c6
THATBLADEBOY <aus10b@gmail.com>
2018-11-26 10:05:31 -0600

    initial commit with completed practice lesson

---
+++ script.js
@@ -0,0 +1,17 @@
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+const pet = {
+    name: "Banksy",
+    species: "Dog",
+    nicknames: [
+        "Cutie",
+        "Patootie",
+        "Pretty Girl"
+    ],
+    age: 7.3,
+};
+
+console.log(pet.name);
